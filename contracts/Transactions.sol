459a4f25712c57464759106b306d33e29a0843e7
Tuag <igortuag@gmail.com>
2022-12-05 08:58:29 -0300

    add transaction event

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -1,2 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+
+contract Transactions {
+  event Transfer(
+    address sender,
+    address receiver,
+    uint amount,
+    string message,
+    uint256 timestamp,
+    string keyword
+  ) {
+  }
+}
