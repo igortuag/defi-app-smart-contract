fa40ab8a01dd019b84a49475a3d81722452202cb
Tuag <igortuag@gmail.com>
2022-12-07 08:51:07 -0300

    add log to deploy contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,8 @@
 const main = async () => {
   const transactionFactory = await hre.ethers.getContractFactory("Transactions");
   const transactionContract = await transactionFactory.deploy();
+
+  await transactionContract.deployed();
+
+  console.log("Contract deployed to:", transactionContract.address);
 };
