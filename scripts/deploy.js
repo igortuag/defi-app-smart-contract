d6aabaf4d89aa6defd9731ee6bd25618e57f43ec
Tuag <igortuag@gmail.com>
2022-12-08 08:15:09 -0300

    add auto invoqued anonymous function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,20 @@
 const main = async () => {
-  const transactionFactory = await hre.ethers.getContractFactory("Transactions");
+  const transactionFactory = await hre.ethers.getContractFactory(
+    "Transactions"
+  );
   const transactionContract = await transactionFactory.deploy();
 
   await transactionContract.deployed();
 
   console.log("Contract deployed to:", transactionContract.address);
 };
+
+(async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+})();
